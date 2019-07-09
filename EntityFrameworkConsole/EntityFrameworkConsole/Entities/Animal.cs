d4d66a92cec41f5283cf3177cd1a12f2425adea5
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-09 18:34:20 +0300

    Add Animal entity.

--- EntityFrameworkConsole/EntityFrameworkConsole/Entities/Animal.cs
+++ EntityFrameworkConsole/EntityFrameworkConsole/Entities/Animal.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityFrameworkConsole.Entities
 {
+    [Table("tblAnimals")]
     public class Animal
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public float Weight { get; set; }
+
+        public string Say { get; set; }
     }
 }
